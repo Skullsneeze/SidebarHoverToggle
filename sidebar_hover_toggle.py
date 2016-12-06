9878e73aca5420187f4ddf9d0181936f0ce5a4d1
Martijn <martiniswink@gmail.com>
2016-12-06 14:10:17 +0100

    Updated some comments

--- sidebar_hover_toggle.py
+++ sidebar_hover_toggle.py
@@ -1,12 +1,13 @@
 import sublime, sublime_plugin
 
 class SidebarToggleListener(sublime_plugin.EventListener):
+    
+    # listen for the hover command so we can toggle the sidebar when hovering over the gutter
     def on_hover(self, view, point, hover_zone):
-        # to not raise an AttributeError Exception
-        # I check if the attribute exists (in both function)
+        # check if the attribute exists to prevent AttributeError Exception
         if not hasattr(self, 'disable_auto_show'):
             self.disable_auto_show = False
-        # if the auto show is disable, stop everything
+        # if the auto show is disabled, stop executing
         if self.disable_auto_show:
             return
 
@@ -17,17 +18,20 @@ class SidebarToggleListener(sublime_plugin.EventListener):
             # hide sidebar
             view.window().set_sidebar_visible(False)
 
+    # listen for every window_command so we know if a user opened the sdiebar manually
     def on_window_command(self, window, command, args):
-        """ listen for every window_command. The one that matters here is `toggle_side_bar` """
+        # check if the attribute exists to prevent AttributeError Exception
         if not hasattr(self, 'disable_auto_show'):
             self.disable_auto_show = False
-
+        
+        # only continue if the command if toggle_side_bar
         if command != 'toggle_side_bar':
             return
+        
         # the side bar is hidden, but because this function is fired
-        # before the command is run, the side bar is going to be in the *opposite*
-        # state than now
+        # before the command is run, the side bar is going to be in the opposite state
         is_going_to_be_visisble = not window.is_sidebar_visible()
+        
         # if this condition is true, it means that the user showed the side bar manually
         # so we disable auto hide/show
         if is_going_to_be_visisble is True:
